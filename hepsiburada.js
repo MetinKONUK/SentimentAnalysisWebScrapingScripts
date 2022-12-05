3469760513c7b0365b48cd1f938d806025b61014
Metin KONUK <metinkonuk@hotmail.com>
2022-12-06 01:50:46 +0300

    refactor hepsiburada scraper

--- hepsiburada.js
+++ hepsiburada.js
@@ -1,21 +1,35 @@
 import puppeteer from "puppeteer";
 
-// crystalin-animal-health-1-lt-yara-bakim-solusyonu-ve-dezenfektan-pm-HB00000EN9C4
-// crystalin-kedi-kopek-icin-deri-ve-meme-bakim-solusyonu-200-ml-p-HBV00000CF6KA
-const scrapeComments = async (productCode) => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
+// list of all products' product code
+const products = [
+    {code: 'crystalin-animal-health-1-lt-yara-bakim-solusyonu-ve-dezenfektan-pm-HB00000EN9C4'},
+    {code: 'crystalin-kedi-kopek-icin-deri-ve-meme-bakim-solusyonu-200-ml-p-HBV00000CF6KA'},
+];
 
-    await page.goto(`https://hepsiburada.com/${productCode}-yorumlari`);
-    await page.waitForSelector('.hermes-FiltersContainerMobile-module-oTmQCnHCBC1J5T7ihRtB');
+// scraper function
+const Scrape = async (code) => {
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
+    await page.goto(`https://hepsiburada.com/${code}-yorumlari`);
+
+    // scrape total reviews page count
+    await page.waitForSelector('.hermes-FiltersContainerMobile-module-oTmQCnHCBC1J5T7ihRtB', {visible: true});
     const pageCount = await page.evaluate(() => {
-        return document.querySelector('.hermes-FiltersContainerMobile-module-oTmQCnHCBC1J5T7ihRtB')
-                       .textContent.split(' ')[4];
+        return Math.ceil((document.querySelector('.hermes-FiltersContainerMobile-module-oTmQCnHCBC1J5T7ihRtB')
+            .textContent.split(' ')[4]) / 10);
     });
-    console.log('Total Page Count: ', Math.ceil(pageCount / 10));
+    console.log('Total Page Count: ', pageCount);
+
+    // scrape product name
+    await page.waitForSelector('.hermes-ProductRate-module-cHmp3strss2sSkhaeXS3 > span', {visible: true});
+    const productName = await page.evaluate(() => {
+        return document.querySelector('.hermes-ProductRate-module-cHmp3strss2sSkhaeXS3 > span').textContent.trim();
+    });
+
     await page.waitForSelector('[itemprop=review]', {visible: true});
-    const data = await page.evaluate(() => {
-        let result = [];
+    const comments = await page.evaluate(() => {
+        let comments = [];
         const reviews = document.querySelectorAll('[itemprop=review]');
         reviews.forEach((review) => {
             let content = review.querySelector('[itemprop=description]').textContent.trim();
@@ -24,24 +38,27 @@ const scrapeComments = async (productCode) => {
             let authorAge = review.querySelectorAll('.hermes-ReviewCard-module-p2lw9pDiloK0sQ9iHHQy > span')[1].textContent.trim();
             let authorCity = review.querySelectorAll('.hermes-ReviewCard-module-p2lw9pDiloK0sQ9iHHQy > span')[2].textContent.trim();
             let rate = review.querySelectorAll('.hermes-RatingPointer-module-UefD0t2XvgGWsKdLkNoX > div').length;
-            result.push({content, date, authorName, authorAge, authorCity, rate});
+            comments.push({content, date, authorName, authorAge, authorCity, rate});
         });
-        return result;
+        return comments;
     });
     await browser.close();
-    return data;
+    return {productName, comments};
 }
 
-// const productCode = 'crystalin-animal-health-1-lt-yara-bakim-solusyonu-ve-dezenfektan-pm-HB00000EN9C4';
-const productCode = 'crystalin-kedi-kopek-icin-deri-ve-meme-bakim-solusyonu-200-ml-p-HBV00000CF6KA';
-scrapeComments(productCode)
-    .then((reviews) => {
-        console.log('Review Count: ', reviews.length);
-        reviews.forEach((review) => {
-        let {content, date, authorName, authorCity, rate} = review;
-            console.log(`Content: ${content}\nDate: ${date}\nAuthor Name: ${authorName}\nAuthor City: ${authorCity}\nRate: ${rate}`);
+// scrape all products' comments
+products.forEach((product) => {
+    Scrape(product.code)
+        .then(data => {
+            const {productName, comments} = data;
+            console.log('Product Name:', productName);
+            console.log('Review Count', comments.length);
+            comments.forEach((comment) => {
+                let {content, date, authorName, authorCity, rate} = comment;
+                console.log(`Author Name: ${authorName}\nAuthor City: ${authorCity}\nDate: ${date}\nContent: ${content}\nRate: ${rate}\n`);
+            });
+        })
+        .catch((err) => {
+            console.log(err)
         });
-    })
-    .catch((err) => {
-        console.log(err)
-    });
+})
