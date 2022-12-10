ef46d0254e3dd6e37518b253d8aa7dfafb8c7e5b
Metin KONUK <metinkonuk@hotmail.com>
2022-12-10 14:27:51 +0300

    fix mistake in rate selector

--- trendyol.js
+++ trendyol.js
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer';
+import * as fs from 'fs';
 
 // list of all products' product code
 const products = [
@@ -8,7 +9,7 @@ const products = [
 
 // scraper function
 const Scrape = async (code) => {
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({headless: false});
     const page = (await browser.pages())[0];
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
     await page.goto(`https://www.trendyol.com/crystalin/${code}/yorumlar`);
@@ -16,13 +17,12 @@ const Scrape = async (code) => {
     // scrape total comments count & product name
     await page.waitForSelector('.pr-rnr-sm-p-s > span', {visible: true});
     const {productName ,commentCount} = await page.evaluate(() => {
-
         let commentCount = Number(document.querySelectorAll('.pr-rnr-sm-p-s > span')[1]
             .innerHTML.split(' ')[0]);
         let productName = document.querySelector('[class=product-name]').textContent;
         return {productName, commentCount};
     });
-    console.log(commentCount);
+    // console.log(commentCount);
     await page.waitForSelector('[class=pr-rnr-com]', {visible: true});
     const comments = await page.evaluate(async (commentCount) => {
         let reviews = document.querySelectorAll('[class=rnr-com-w]');
@@ -31,8 +31,8 @@ const Scrape = async (code) => {
             console.log('Scrolled', scrollAmount);
             window.scrollBy(0, scrollAmount);
             await new Promise(resolve => {
-                setTimeout(resolve, 1000)
-            })
+                setTimeout(resolve, 100);
+            });
             if(reviews.length !== document.querySelectorAll('[class=rnr-com-w]').length){
                 scrollAmount += 100;
             }
@@ -42,7 +42,7 @@ const Scrape = async (code) => {
         let comments = [];
         reviews.forEach(review => {
             let content = review.querySelector('p').textContent.trim();
-            let rate = review.querySelectorAll('.ratings > div').length;
+            let rate = 5 - review.querySelectorAll(".ratings .full[style*='width: 0%;'], .ratings .full[style*='width: 0px;']").length;
             let details = review.querySelector('[class=rnr-com-usr]').textContent.trim().split('|');
             let authorName = details[0];
             let date = details[1];
@@ -59,13 +59,23 @@ const Scrape = async (code) => {
 products.forEach(product => {
     let {code} = product;
     Scrape(code).then(({productName, comments}) => {
-        console.log('Product Name: ', productName);
-        console.log('Total Comments Count: ', comments.length);
-        comments.forEach(comment => {
-            let {authorName, date, content, rate, vendorName} = comment;
-            console.log(`Author Name: ${authorName}\nDate: ${date}\nContent: ${content}\nRate: ${rate}\nVendor Name: ${vendorName}\n`);
+        const jsonContent = JSON.stringify(comments, null, '\t');
+
+        fs.writeFile(`./${productName}.json`, jsonContent, 'utf8', function (err) {
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log("The file was saved!");
         });
+
+        // console.log('Product Name: ', productName);
+        // console.log('Total Comments Count: ', comments.length);
+        // comments.forEach(comment => {
+        //     let {authorName, date, content, rate, vendorName} = comment;
+        //     console.log(`Author Name: ${authorName}\nDate: ${date}\nContent: ${content}\nRate: ${rate}\nVendor Name: ${vendorName}\n`);
+        // });
     }).catch(err => {
         console.log(err)
     });
-})
+});
