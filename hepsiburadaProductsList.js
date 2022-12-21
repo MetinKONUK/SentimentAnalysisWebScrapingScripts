d05589aa000847a861628036a97c8737289f79f1
Metin KONUK <metinkonuk@hotmail.com>
2022-12-21 18:48:17 +0300

    uncomment the commented product datas to include them in scraping process

--- hepsiburadaProductsList.js
+++ hepsiburadaProductsList.js
@@ -7,106 +7,106 @@ const products = [
         code: 'crystalin-animal-health-1-lt-yara-bakim-solusyonu-ve-dezenfektan-p-HBV00000EN9C5',
         category: 'animal-health-1000-ml'
     },
-    // {
-    //     code: 'crystalin-1-lt-goz-kulak-ve-yara-solisyonu-p-HBV00000VCJ29',
-    //     category: 'animal-health-1000-ml'
-    // },
-    // {
-    //     code: 'crystalin-animal-health-yara-temizleme-antiseptigi-100-ml-p-HBCV000012VZJC',
-    //     category: 'animal-health-100-ml'
-    // },
-    // {
-    //     code: 'crystalin-dezenfektan-1-lt-p-HBV00000SZDEF',
-    //     category: 'antiseptic-sanitizer-1000-ml'
-    // },
-    // {
-    //     code: 'yara-bakim-solusyonu-crystalin-400ml-p-HBV0000118WJP',
-    //     category: 'antiseptic-sanitizer-400-ml'
-    // },
-    // {
-    //     code: 'crystalin-400-ml-dezenfektan-sprey-baslikli-p-HBCV000022HU2O',
-    //     category: 'antiseptic-sanitizer-400-ml'
-    // },
-    // {
-    //     code: 'crystalin-animal-health-50-ml-kedi-ve-kopek-goz-kulak-agiz-bakim-spreyi-50-ml-p-HBCV00002JNGF2',
-    //     category: 'animal-health-50-ml'
-    // },
-    // {
-    //     code: 'crystalin-400-ml-dezenfektan-sprey-baslik-p-HBCV000002MJFL',
-    //     category: 'antiseptic-sanitizer-400-ml'
-    // },
-    // {
-    //     code: 'yara-bakim-solusyonu-crystalin-200-ml-p-HBV000011UIEY',
-    //     category: 'antiseptic-sanitizer-200-ml'
-    // },
-    // {
-    //     code: 'crystalin-eco-5-lt-10-x-500-ml-el-cilt-ortam-dezenfektani-dezenfektan-sprey-baslikli-dispenser-p-HBCV000003MCQJ',
-    //     category: 'hygienic-aid-kit'
-    // },
-    // {
-    //     code: 'crystalin-animal-health-200-ml-kedi-ve-kopekler-icin-goz-kulak-agiz-bakim-solusyonu-p-HBCV00002WNQNS',
-    //     category: 'animal-health-200-ml'
-    // },
-    // {
-    //     code: 'crystalin-eco-dezenfektan-hijyenik-yasam-kiti-p-HBV00000WFCYG',
-    //     category: 'hygienic-aid-kit'
-    // },
-    // {
-    //     code: 'crystalin-eco-2-lt-10-x-200-ml-el-cilt-ortam-dezenfektani-sprey-baslikli-dispenser-p-HBCV000003MCQH',
-    //     category: 'hygienic-aid-kit'
-    // },
-    // {
-    //     code: 'crystalin-animal-health-yara-bakim-solusyonu-1-lt-p-HBCV00000EBP4U',
-    //     category: 'animal-health-1000-ml'
-    // },
-    // {
-    //     code: 'crystalin-dezenfektan-sprey-100-ml-p-HBV00000SZDEH',
-    //     category: 'antiseptic-sanitizer-100-ml'
-    // },
-    // {
-    //     code: 'crystalin-animal-health-50-ml-kedi-ve-kopek-goz-kulak-agiz-bakim-p-HBCV00002WN5NM',
-    //     category: 'animal-health-50-ml'
-    // },
-    // {
-    //     code: 'crystalin-gargara-solusyonu-100-ml-p-HBV00000SVMT5',
-    //     category: 'mouthwash-solution'
-    // },
-    // {
-    //     code: 'crystalin-animal-health-5-lt-yara-deri-ve-meme-bakim-solusyonu-dezenfektan-p-HBV00000EN9C7',
-    //     category: 'animal-health-5000-ml'
-    // },
-    // {
-    //     code: 'crystalin-animal-health-yara-temizleme-antiseptigi-5-lt-p-HBV00000WGF1U',
-    //     category: 'animal-health-5000-ml'
-    // },
-    // {
-    //     code: 'crystalin-kedi-3-lu-ihtiyac-seti-101-crystalin-animal-healt-kedi-kopek-agiz-ve-goz-solusyonu-paspas-eldiven-p-HBCV00002H0AA1',
-    //     category: 'trinity-aid-kit-0'
-    // },
-    // {
-    //     code: 'crystalin-dezenfektan-5-lt-p-HBV00000SZDED',
-    //     category: 'animal-health-5000-ml'
-    // },
-    // {
-    //     code: 'crystalin-kedi-3-lu-ihtiyac-seti-110-animal-kedi-kopek-agiz-ve-goz-solusyonu-tirnak-makasi-tarak-crystalin-p-HBCV00002H09Z2',
-    //     category: 'trinity-aid-kit-1'
-    // },
-    // {
-    //     code: 'crystalin-kedi-3-lu-ihtiyac-seti-102-animal-kedi-kopek-agiz-ve-goz-solusyonu-tarak-tirnak-makasi-crystalin-p-HBCV00002H09YU',
-    //     category: 'trinity-aid-kit-1'
-    // },
-    // {
-    //     code: 'crystalin-kedi-3-lu-ihtiyac-seti-112-animal-kedi-kopek-agiz-ve-goz-solusyonu-tirnak-makasi-eldiven-crystalin-p-HBCV00002H0A9D',
-    //     category: 'trinity-aid-kit-1'
-    // },
-    // {
-    //     code: 'crystalin-animal-health-yara-temizleme-antiseptigi-1-lt-p-HBV00000WGF26',
-    //     category: 'animal-health-1000-ml'
-    // },
-    // {
-    //     code: 'crystalin-dezenfektan-500-ml-p-HBCV00002LE2AV',
-    //     category: 'antiseptic-sanitizer-500-ml'
-    // }
+    {
+        code: 'crystalin-1-lt-goz-kulak-ve-yara-solisyonu-p-HBV00000VCJ29',
+        category: 'animal-health-1000-ml'
+    },
+    {
+        code: 'crystalin-animal-health-yara-temizleme-antiseptigi-100-ml-p-HBCV000012VZJC',
+        category: 'animal-health-100-ml'
+    },
+    {
+        code: 'crystalin-dezenfektan-1-lt-p-HBV00000SZDEF',
+        category: 'antiseptic-sanitizer-1000-ml'
+    },
+    {
+        code: 'yara-bakim-solusyonu-crystalin-400ml-p-HBV0000118WJP',
+        category: 'antiseptic-sanitizer-400-ml'
+    },
+    {
+        code: 'crystalin-400-ml-dezenfektan-sprey-baslikli-p-HBCV000022HU2O',
+        category: 'antiseptic-sanitizer-400-ml'
+    },
+    {
+        code: 'crystalin-animal-health-50-ml-kedi-ve-kopek-goz-kulak-agiz-bakim-spreyi-50-ml-p-HBCV00002JNGF2',
+        category: 'animal-health-50-ml'
+    },
+    {
+        code: 'crystalin-400-ml-dezenfektan-sprey-baslik-p-HBCV000002MJFL',
+        category: 'antiseptic-sanitizer-400-ml'
+    },
+    {
+        code: 'yara-bakim-solusyonu-crystalin-200-ml-p-HBV000011UIEY',
+        category: 'antiseptic-sanitizer-200-ml'
+    },
+    {
+        code: 'crystalin-eco-5-lt-10-x-500-ml-el-cilt-ortam-dezenfektani-dezenfektan-sprey-baslikli-dispenser-p-HBCV000003MCQJ',
+        category: 'hygienic-aid-kit'
+    },
+    {
+        code: 'crystalin-animal-health-200-ml-kedi-ve-kopekler-icin-goz-kulak-agiz-bakim-solusyonu-p-HBCV00002WNQNS',
+        category: 'animal-health-200-ml'
+    },
+    {
+        code: 'crystalin-eco-dezenfektan-hijyenik-yasam-kiti-p-HBV00000WFCYG',
+        category: 'hygienic-aid-kit'
+    },
+    {
+        code: 'crystalin-eco-2-lt-10-x-200-ml-el-cilt-ortam-dezenfektani-sprey-baslikli-dispenser-p-HBCV000003MCQH',
+        category: 'hygienic-aid-kit'
+    },
+    {
+        code: 'crystalin-animal-health-yara-bakim-solusyonu-1-lt-p-HBCV00000EBP4U',
+        category: 'animal-health-1000-ml'
+    },
+    {
+        code: 'crystalin-dezenfektan-sprey-100-ml-p-HBV00000SZDEH',
+        category: 'antiseptic-sanitizer-100-ml'
+    },
+    {
+        code: 'crystalin-animal-health-50-ml-kedi-ve-kopek-goz-kulak-agiz-bakim-p-HBCV00002WN5NM',
+        category: 'animal-health-50-ml'
+    },
+    {
+        code: 'crystalin-gargara-solusyonu-100-ml-p-HBV00000SVMT5',
+        category: 'mouthwash-solution'
+    },
+    {
+        code: 'crystalin-animal-health-5-lt-yara-deri-ve-meme-bakim-solusyonu-dezenfektan-p-HBV00000EN9C7',
+        category: 'animal-health-5000-ml'
+    },
+    {
+        code: 'crystalin-animal-health-yara-temizleme-antiseptigi-5-lt-p-HBV00000WGF1U',
+        category: 'animal-health-5000-ml'
+    },
+    {
+        code: 'crystalin-kedi-3-lu-ihtiyac-seti-101-crystalin-animal-healt-kedi-kopek-agiz-ve-goz-solusyonu-paspas-eldiven-p-HBCV00002H0AA1',
+        category: 'trinity-aid-kit-0'
+    },
+    {
+        code: 'crystalin-dezenfektan-5-lt-p-HBV00000SZDED',
+        category: 'animal-health-5000-ml'
+    },
+    {
+        code: 'crystalin-kedi-3-lu-ihtiyac-seti-110-animal-kedi-kopek-agiz-ve-goz-solusyonu-tirnak-makasi-tarak-crystalin-p-HBCV00002H09Z2',
+        category: 'trinity-aid-kit-1'
+    },
+    {
+        code: 'crystalin-kedi-3-lu-ihtiyac-seti-102-animal-kedi-kopek-agiz-ve-goz-solusyonu-tarak-tirnak-makasi-crystalin-p-HBCV00002H09YU',
+        category: 'trinity-aid-kit-1'
+    },
+    {
+        code: 'crystalin-kedi-3-lu-ihtiyac-seti-112-animal-kedi-kopek-agiz-ve-goz-solusyonu-tirnak-makasi-eldiven-crystalin-p-HBCV00002H0A9D',
+        category: 'trinity-aid-kit-1'
+    },
+    {
+        code: 'crystalin-animal-health-yara-temizleme-antiseptigi-1-lt-p-HBV00000WGF26',
+        category: 'animal-health-1000-ml'
+    },
+    {
+        code: 'crystalin-dezenfektan-500-ml-p-HBCV00002LE2AV',
+        category: 'antiseptic-sanitizer-500-ml'
+    }
 ];
 
 export { products };
--- trendyolProductsList.js
+++ trendyolProductsList.js
@@ -7,82 +7,82 @@ const products = [
         code: 'animal-health-50ml-p-330617713',
         category: 'animal-health-50-ml',
     },
-    // {
-    //     code: '200-ml-antiseptik-dezenfektan-sprey-p-22714059',
-    //     category: 'antiseptic-sanitizer-200-ml',
-    // },
-    // {
-    //     code: 'astgrosspet-goz-kulak-ve-agiz-bakim-solusyonu-50-ml-p-277111474',
-    //     category: 'animal-health-50-ml',
-    // },
-    // {
-    //     code: '200-ml-animal-health-p-364320758',
-    //     category: 'animal-health-200-ml',
-    // },
-    // {
-    //     code: '400-ml-antiseptik-dezenfektan-p-22714459',
-    //     category: 'antiseptic-sanitizer-400-ml',
-    // },
-    // {
-    //     code: '1-litre-antiseptik-dezenfektan-p-48308991',
-    //     category: 'antiseptic-sanitizer-1000-ml'
-    // },
-    // {
-    //     code: 'animal-health-kopek-goz-kulak-agiz-bakim-solusyonu-200-ml-p-346119543',
-    //     category: 'animal-health-200-ml',
-    // },
-    // {
-    //     code: 'antiseptik-dezenfektan-100-ml-sprey-p-36793194',
-    //     category: 'antiseptic-sanitizer-100-ml'
-    // },
-    // {
-    //     code: 'kedi-kopek-agiz-bakim-p-316678503',
-    //     category: 'animal-health-200-ml',
-    // },
-    // {
-    //     code: 'animal-health-50-ml-kedi-ve-kopek-goz-kulak-agiz-bakim-spreyi-50-ml-p-178495588',
-    //     category: 'animal-health-50-ml',
-    // },
-    // {
-    //     code: 'kedi-kopek-agiz-bakim-solusyon-1-litre-p-342294613',
-    //     category: 'animal-health-1000-ml'
-    // },
-    // {
-    //     code: '50-ml-antiseptik-spreyi-p-22713809',
-    //     category: 'antiseptic-sanitizer-50-ml'
-    // },
-    // {
-    //     code: 'kedi-kopek-bakim-spreyi-p-345542435',
-    //     category: 'animal-health-200-ml',
-    // },
-    // {
-    //     code: 'animal-yara-goz-kulak-ve-burun-temizleme-solusyonu-100-ml-p-169027022',
-    //     category: 'animal-health-100-ml',
-    // },
-    // {
-    //     code: 'animal-health-200-ml-agiz-bakim-spreyi-ml-p-316750301',
-    //     category: 'animal-health-200-ml',
-    // },
-    // {
-    //     code: 'kedi-kopek-icin-goz-kulak-ve-agiz-bakim-solusyonu-200-ml-p-341218649',
-    //     category: 'animal-health-200-ml',
-    // },
-    // {
-    //     code: 'antiseptik-dezenfektan-400-ml-p-280709872',
-    //     category: 'antiseptic-sanitizer-400-ml',
-    // },
-    // {
-    //     code: 'antiseptik-dezenfektan-5-lt-p-39703954',
-    //     category: 'antiseptic-sanitizer-5000-ml'
-    // },
-    // {
-    //     code: 'animal-health-5-lt-p-40530855',
-    //     category: 'animal-health-5000-ml',
-    // },
-    // {
-    //     code: 'animal-health-5-lt-dezenfektan-p-332339320',
-    //     category: 'animal-health-5000-ml',
-    // },
+    {
+        code: '200-ml-antiseptik-dezenfektan-sprey-p-22714059',
+        category: 'antiseptic-sanitizer-200-ml',
+    },
+    {
+        code: 'astgrosspet-goz-kulak-ve-agiz-bakim-solusyonu-50-ml-p-277111474',
+        category: 'animal-health-50-ml',
+    },
+    {
+        code: '200-ml-animal-health-p-364320758',
+        category: 'animal-health-200-ml',
+    },
+    {
+        code: '400-ml-antiseptik-dezenfektan-p-22714459',
+        category: 'antiseptic-sanitizer-400-ml',
+    },
+    {
+        code: '1-litre-antiseptik-dezenfektan-p-48308991',
+        category: 'antiseptic-sanitizer-1000-ml'
+    },
+    {
+        code: 'animal-health-kopek-goz-kulak-agiz-bakim-solusyonu-200-ml-p-346119543',
+        category: 'animal-health-200-ml',
+    },
+    {
+        code: 'antiseptik-dezenfektan-100-ml-sprey-p-36793194',
+        category: 'antiseptic-sanitizer-100-ml'
+    },
+    {
+        code: 'kedi-kopek-agiz-bakim-p-316678503',
+        category: 'animal-health-200-ml',
+    },
+    {
+        code: 'animal-health-50-ml-kedi-ve-kopek-goz-kulak-agiz-bakim-spreyi-50-ml-p-178495588',
+        category: 'animal-health-50-ml',
+    },
+    {
+        code: 'kedi-kopek-agiz-bakim-solusyon-1-litre-p-342294613',
+        category: 'animal-health-1000-ml'
+    },
+    {
+        code: '50-ml-antiseptik-spreyi-p-22713809',
+        category: 'antiseptic-sanitizer-50-ml'
+    },
+    {
+        code: 'kedi-kopek-bakim-spreyi-p-345542435',
+        category: 'animal-health-200-ml',
+    },
+    {
+        code: 'animal-yara-goz-kulak-ve-burun-temizleme-solusyonu-100-ml-p-169027022',
+        category: 'animal-health-100-ml',
+    },
+    {
+        code: 'animal-health-200-ml-agiz-bakim-spreyi-ml-p-316750301',
+        category: 'animal-health-200-ml',
+    },
+    {
+        code: 'kedi-kopek-icin-goz-kulak-ve-agiz-bakim-solusyonu-200-ml-p-341218649',
+        category: 'animal-health-200-ml',
+    },
+    {
+        code: 'antiseptik-dezenfektan-400-ml-p-280709872',
+        category: 'antiseptic-sanitizer-400-ml',
+    },
+    {
+        code: 'antiseptik-dezenfektan-5-lt-p-39703954',
+        category: 'antiseptic-sanitizer-5000-ml'
+    },
+    {
+        code: 'animal-health-5-lt-p-40530855',
+        category: 'animal-health-5000-ml',
+    },
+    {
+        code: 'animal-health-5-lt-dezenfektan-p-332339320',
+        category: 'animal-health-5000-ml',
+    },
 ];
 
 export { products };
